79a4a261a2efe05ee00f738a866c812b307c7caa
Arvid <axar1802@student.ju.se>
2022-11-10 20:57:27 +0100

    Initial Commit

---
+++ Adv SwiftUI Workshop/Adv_SwiftUI_WorkshopApp.swift
@@ -0,0 +1,17 @@
+//
+//  Adv_SwiftUI_WorkshopApp.swift
+//  Adv SwiftUI Workshop
+//
+//  Created by Arvid Axelsson on 2022-11-10.
+//
+
+import SwiftUI
+
+@main
+struct Adv_SwiftUI_WorkshopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
